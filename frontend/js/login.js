0574b8690faef9344e37170fbc3ce3b51452949c
Bobino6 <posibilidades3d@gmail.com>
2025-06-11 13:55:04 +0200

    Update login.js
    update2

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -19,7 +19,7 @@ const form = document.getElementById('login-form');
         localStorage.setItem("EmployeeId", data.id);
         localStorage.setItem("EmployeeName", data.name);
         // Redirigir a clients.html
-        window.location.href = 'D:/Proyecto-Edu-app/frontend/pages/clientes.html';
+        window.location.href = 'clientes.html';
       } else {
         alert('Correo o contraseña incorrectos.');
       }
